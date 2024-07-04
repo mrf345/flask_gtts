bae7ef010a3e6892e9d1cd4bad8abb50e7fe42d6
Mohamed Feddad <mrf345@gmail.com>
2024-07-04 23:54:04 +0400

    chore: update setup config

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from os import path
 from setuptools import setup
 
-
+supported_versions = ["3.7", "3.8", "3.9", "3.10", "3.11", "3.12"]
 basedir = path.abspath(path.dirname(__file__))
 long_description = ''
 requirements = []
-test_requirements = []
 requirements_path = path.join(basedir, 'requirements')
 
 
@@ -15,15 +14,21 @@ with open(path.join(basedir, path.join('flask_gtts', 'about.py'))) as f:
 with open(path.join(basedir, 'README.md')) as f:
     long_description += f.read()
 
-with open(path.join(requirements_path, 'main.txt')) as f:
-    requirements += [line for line in f.read().split('\n') if line]
-    test_requirements += requirements
 
-with open(path.join(requirements_path, 'test.txt')) as f:
-    test_requirements += [
-        line for line in f.read().split('\n')
-        if line and not line.startswith('-r')]
+if path.isdir(requirements_path):
+    with open(path.join(requirements_path, 'main.txt')) as f:
+        requirements += [line for line in f.read().split('\n') if line]
+else:
+    requires_path = path.join(
+        path.join(basedir, "Flask_gTTS.egg-info"), "requires.txt"
+    )
 
+    with open(requires_path) as f:
+        requirements += [line for line in f.read().split("\n") if line]
+
+supported_python_classifiers = [
+    "Programming Language :: Python :: {0}".format(v) for v in supported_versions
+]
 
 setup(
     name='Flask-gTTS',
@@ -41,16 +46,11 @@ setup(
     zip_safe=False,
     platforms='any',
     install_requires=requirements,
-    setup_requires=test_requirements,
     package_data={'flask_gtts': ['read.html']},
     keywords=['flask', 'extension', 'google', 'text', 'speech',
               'gTTS', 'TTS', 'text-to-speech'],
     classifiers=[
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        *supported_python_classifiers,
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
